fca23b91b44c962ed8d8042ce531a0ad5400ac4d
pabloferreri <pab.ferreri@gmail.com>
2021-02-25 18:44:19 -0300

    detail page + edits

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -8,10 +8,18 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productsController = {
     index : function(req,res) {
-        return res.render('products/product-list',{products: products, title : "Producto", stylesheet: "product-list.css"})
+        return res.render('products/product-list',{products: products, title : "Productos", stylesheet: 'product-list.css'})
     },
-    detail : function (req,res) {
-        return res.render('products/detail', {products: products, title : "Detalle del prodcto", stylesheet : "detail.css"})
+    detail : (req, res) => {
+		const product = products.find(product => product.id == req.params.id);
+
+		if (product) {
+			return res.render('products/detail',{product: product, title : "Detalle del prodcto", stylesheet: 'detail.css'})
+		}else{
+			return res.send("error");
+		}
+		
+        //return res.render('products/detail',{products: products, title : "Detalle del prodcto", stylesheet: 'detail.css'})
     }
 }
 
