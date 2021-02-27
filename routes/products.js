1d538dae501114a135c077cd888909b52045d3e5
pabloferreri <pab.ferreri@gmail.com>
2021-02-27 00:57:12 -0300

    edit and create pages, multer, method-override

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
-const products = require('../controllers/productsController');
+
+//----------- multer require ------------------
+const multer = require('multer');
+
+let multerDiskStorege = multer.diskStorage({
+
+    destination: (req,file,cb)=>{
+        let folder = path.join(__dirname,"../public/images/products");
+        cb(null,folder);
+    },
+    filename: (req,file,cb)=>{
+        let imageName = Date.now() + path.extname(file.originalname);
+        cb(null,imageName);
+    }
+
+})
+
+let fileUpload = multer({storege: multerDiskStorege});
+
+//----------- product Controller require ------
+const productsController = require('../controllers/productsController');
 
 /* GET home page. */
-router.get('/', products.index);
-router.get('/detalle/:id', products.detail);
-router.get('/crear', products.showCreate);
-router.get('/edit', products.showCreate);
+router.get('/', productsController.index);
+
+//----------- product detail -----------------
+router.get('/detalle/:id', productsController.detail);
+
+//----------- create product -----------------
+router.get('/crear', productsController.create);
+router.post('/crear', fileUpload.single("image"),productsController.store); 
+
+//----------- edit product -------------------
+router.get('/:id/edit', productsController.edit);
+router.put('/edit/:id',fileUpload.single("image"),productsController.update)
 
 module.exports = router;
