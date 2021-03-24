d5a6ca7ca16bdd4fcec040c3cf1b48dfc4bd1ef2
Feli02 <felipeberraz02@gmail.com>
2021-03-24 11:56:30 -0300

    edito users routes

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,11 @@ const users = require('../controllers/usersController');
 /* GET users listing. */
 router.get('/registro', users.register);
 router.get('/iniciar-sesion', users.login);
+router.get('/perfil', users.profile);
+router.get('/editar', users.edit);
+router.post('/guardar', users.save);
+router.put('/perfil', users.update);
+router.post('/acceder', users.access);
+router.delete('/desactivar', users.delete);
 
 module.exports = router;
