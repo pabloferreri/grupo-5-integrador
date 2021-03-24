3f482bed48c926414376f5061d5df89c1cb4a3fc
Feli02 <felipeberraz02@gmail.com>
2021-03-24 11:50:54 -0300

    edito usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const userFilePath = path.resolve(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+
+
 const usersController = {
-    register : function(req,res) {
-        return res.render('users/register', {title : "Registrarse", stylesheet: "register.css"})
+    login:(req,res)=>{
+        return res.render('users/login', {title : "Iniciar Sesión",stylesheet : "login.css"});
     },
-    login : function(req,res) {
-        return res.render('users/login', {title : "Iniciar Sesión",stylesheet : "login.css"})
-    }
+    register:(req,res)=>{
+        return res.render('users/register', {title : "Registrarse", stylesheet: "register.css"});
+    },
+    access: (req,res)=>{
+
+    },
+    save: (req,res)=>{
+
+    },
+    profile: (req,res)=>{
+
+    },
+    edit: (req,res)=>{
+
+    },
+    update: (req,res)=>{
+
+    },
+    delete: (req,res)=>{
+
+    },
 }
 
 module.exports = usersController;
