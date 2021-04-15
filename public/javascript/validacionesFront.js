df648ccae41aecf7729ea6962b32d8e5f95a461b
Feli02 <felipeberraz02@gmail.com>
2021-04-15 13:48:09 -0300

    js en el front

--- public/javascript/validacionesFront.js
+++ public/javascript/validacionesFront.js
@@ -1,57 +1,74 @@
 window.addEventListener('load', function(){
     
-        let formulario = document.querySelector('#formRegister')
-        let inputName = document.querySelector('#name');
-        let inputApellido = document.querySelector('#lastname');
-        let inputPhone = document.querySelector('#phone');
-        let inputPassword = document.querySelector('#password');
-        let inputPassConfirmation = document.querySelector('#passwordConfirmation');
-        let errors = [];
+    let formulario = document.querySelector('#formRegister')
+    let inputName = document.querySelector('#name');
+    let inputApellido = document.querySelector('#lastname');
+    let inputPhone = document.querySelector('#phone');
+    let inputPassword = document.querySelector('#password');
+    let inputPassConfirmation = document.querySelector('#passwordConfirmation');
+    let errors = [];
 
-        inputName.addEventListener('blur', function(){
-            if(typeof inputName != "string"){
-                errors.push('error en el campo de nombre')
-                alert('Uupss! Parece que se ha producido un error en el campo nombre')
-            }
-        });
-        inputApellido.addEventListener('blur', function(){
-            if(typeof inputApellido != "string"){
-                errors.push('error en el campo de apellido');
-                alert('Uupss! Parece que se ha producido un error en el campo apellido')
-            }
-        });
+    inputName.addEventListener('blur', function(){
+        if(!isNaN(inputName.value)){
+            errors.push('error en el campo de nombre');
+            inputName.style.border = "solid red 1px";
+            
+            alert('Uupss! Parece que se ha producido un error en el campo nombre');
+        }else{
+            inputName.style.border = "solid green 1px";
+        }
+    });
+    inputApellido.addEventListener('blur', function(){
+        if(!isNaN(inputApellido.value)){
+            errors.push('error en el campo de apellido');
+            
+            inputApellido.style.border = "solid red 1px";
+            alert('Uupss! Parece que se ha producido un error en el campo apellido');
+        }else{
+            inputApellido.style.border = "solid green 1px";
+        }
+    });
 
-        document.getElementById('email').addEventListener('blur', function() {
-            campo = event.target;    
-            emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-            //Se muestra un texto a modo de ejemplo, luego va a ser un icono
-            if (!emailRegex.test(campo.value)) {
-                errors.push('error en el campo de email')
-                alert('Uupss! Parece que se ha producido un error en el campo email')
-            }
-        });
+    document.getElementById('email').addEventListener('blur', function() {
+        campo = event.target;    
+        emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+        if (!emailRegex.test(campo.value)) {
+            errors.push('error en el campo de email')
+            email.style.border = "solid red 1px";
+            alert('Uupss! Parece que se ha producido un error en el campo email')
+        }else{
+            email.style.border = "solid green 1px";
+        }
+    });
 
-        inputPhone.addEventListener('blur', function(){
-            if(isNaN(inputPhone.value)){
-                errors.push('error en el campo de teléfono')
-                alert('Uupss! Parece que se ha producido un error en el campo de teléfono, prueba sin espacios ni otro caracter')
-            }
-        });
-        inputPassConfirmation.addEventListener('blur', function(){
-            if(inputPassword.value != inputPassConfirmation.value){
-                errors.push('error las passwords no coinciden')
-                alert('Uupss! Parece que las contraseñas no coinciden')
-            }
-        });
+    inputPhone.addEventListener('blur', function(){
+        if(isNaN(inputPhone.value)){
+            errors.push('error en el campo de teléfono')
+            inputPhone.style.border = "solid red 1px";
+            
+            alert('Uupss! Parece que se ha producido un error en el campo de teléfono, prueba sin espacios ni otro caracter')
+        }else{
+            inputPhone.style.border = "solid green 1px";
+            
+        }
+    });
+    inputPassConfirmation.addEventListener('blur', function(){
+        if(inputPassword.value != inputPassConfirmation.value){
+            errors.push('error las passwords no coinciden')
+            inputPassConfirmation.style.border = "solid red 1px";
+            alert('Uupss! Parece que las contraseñas no coinciden')
+        }else{
+            inputPassConfirmation.style.border = "solid green 1px";
+        }
+    });
+
     
-        
 
-        formulario.addEventListener('submit', function(event){
-            if (errors.length > 0){
-                event.preventDefault();
-            }
-            alert('Uupss! Parcere que ha habido un error en los siguientes campos: ' + errors)
-        })
+    formulario.addEventListener('submit', function(event){
+        if (errors.length > 0){
+            event.preventDefault();
+        }
+        alert('Uupss! Parcere que ha habido un error en los siguientes campos: ' + errors)
     })
-
-
+})
